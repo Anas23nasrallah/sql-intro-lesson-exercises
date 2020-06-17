0b73936ad4e7837315e9ce0f740f3289b3efac79
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-06-17 16:14:12 +0300

    exs Done

--- solutions/ex_1.sql
+++ solutions/ex_1.sql
@@ -1,1 +1,18 @@
 USE sql_intro;
+
+CREATE TABLE Dolphin(
+    name VARCHAR(20) NOT NULL PRIMARY KEY,
+    color VARCHAR(12),
+    height INT,
+    healthy BOOLEAN
+); 
+
+INSERT INTO Dolphin VALUES('Daron', 'red', 12, 1);
+INSERT INTO Dolphin VALUES('Maayan', 'blue', 1, 1);
+INSERT INTO Dolphin VALUES('Netta', 'red', 12, 1);
+INSERT INTO Dolphin VALUES('Steve', 'green', 12, 1);
+INSERT INTO Dolphin VALUES('Drogon', 'red', 12, 1);
+INSERT INTO Dolphin VALUES('Rhaegal', 'green', 10, 1);
+INSERT INTO Dolphin VALUES('Viserion', 'yellow', 11, 1);
+
+SELECT * FROM Dolphin;
